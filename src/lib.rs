1afbfc3c713afcb68603708cff18d47a55506fc6
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2021-11-25 00:06:14 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -10,69 +10,58 @@ use numpy::{c64, IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
 // A base line implementation for konnoohmachi spectral filter with a python
 // interface.
 
-fn smoothing_window(freqs: ArrayViewMutD<'_, f64>, f_corner: f64, b: f64) {
+fn smoothing_window(mut freqs: ArrayViewMutD<'_, f64>, f_corner: f64, b: f64) {
     // Note that there HAS to be a zero frequency at the moment!
     let error_margin = 0.000000001;
 
     let index_zero_freq = freqs.iter().position(|&f| f == 0.0).unwrap();
     let index_f_corner = freqs.iter().position(|&f| (f - f_corner).abs() < error_margin).unwrap();
 
-    // return freqs just for testing
-    // &freqs  * b
-    // if f_corner == 0.0 {
-    //     let mut window = vec![0.; freqs.len()];
-    //     window[index_zero_freq] = 1.;
-    //     window
-    // } else {
-    println!("f_corner: {} b: {}", f_corner, b);
-    // let scaled = (&freqs / f_corner) * b;
-    // for (ival, val) in scaled.iter().enumerate() {
-    //     println!("scaled: {} unscaled {}", val, &freqs[ival]);
-    // }
-    &freqs.iter()
-        .map(|freq| f64::log10(freq / f_corner) * b)
-        .map(|w| f64::powi(f64::sin(w) / w, 4));
-    // freqs[index_f_corner] = 1.;
-    // freqs[index_zero_freq] = 0.;
-    // let normalization: f64 = freqs.iter().sum();
-    // freqs.iter_mut().for_each(|x| *x /= normalization);
-    
+    if f_corner == 0.0 {
+        let mut window = vec![0.; freqs.len()];
+        freqs[index_zero_freq] = 1.;
+    } else {
+        freqs.iter_mut()
+            .for_each(|freq| *freq = f64::log10(*freq / f_corner) * b);
+        freqs.iter_mut()
+            .for_each(|w| *w = f64::powi(f64::sin(*w) / *w, 4));
+        freqs[index_f_corner] = 1.;
+        freqs[index_zero_freq] = 0.;
+        let normalization: f64 = freqs.iter().sum();
+        freqs.iter_mut().for_each(|x| *x /= normalization);
+    }
 }
 
-pub fn konnoohmachi_smooth(freqs: ArrayViewD<'_, f64>, amps: ArrayViewD<'_, f64>, b: f64) ->  ArrayD<f64> {
-    // return freqs for testing
-    b * &freqs
-    // let n_freqs = freqs.len();
-    // let mut smoothed = vec![0.; n_freqs];
+pub fn konnoohmachi_smooth(mut freqs: ArrayViewD<'_, f64>, amps: ArrayViewD<'_, f64>, b: f64) {
+    let n_freqs = freqs.len();
+    let mut smoothed = vec![0.; n_freqs];
 
-    // let freqs_iter = freqs.clone();
-    // for (i_freq, f_corner) in freqs_iter.iter().enumerate() {
-    //     let window = smoothing_window(&freqs, *f_corner, b);
+    let freqs_work = freqs.clone();
+    let freqs_iter = freqs.clone();
+    for (i_freq, f_corner) in freqs_iter.iter().enumerate() {
+        let mut window = freqs_work.clone();
+        smoothing_window(window, *f_corner, b);
 
-    //     let product: f64 = window.iter().zip(amps.iter()).map(|(x, y)| x * y).sum();
+        let product: f64 = window.iter().zip(amps.iter()).map(|(x, y)| x * y).sum();
 
-    //     smoothed[i_freq] = product;
-    // }
-    // smoothed
+        freqs[i_freq] = product;
+    }
 }
 
 #[pymodule]
 fn konnoohmachi(_py: Python, m: &PyModule) -> PyResult<()> {
 
     #[pyfn(m)] #[pyo3(name = "smooth")]
-    fn smooth_py<'py>(py: Python<'py>, freqs: PyReadonlyArrayDyn<'_, f64>, amps: PyReadonlyArrayDyn<'_, f64>, b: f64) -> &'py PyArrayDyn<f64> {
+    fn smooth_py<'py>(py: Python<'py>, freqs: PyReadonlyArrayDyn<'_, f64>, amps: PyReadonlyArrayDyn<'_, f64>, b: f64) {
         let freqs = freqs.as_array();
         let amps = amps.as_array();
-        let smoothed = konnoohmachi_smooth(freqs, amps, b);
-        smoothed.into_pyarray(py)
+        konnoohmachi_smooth(freqs, amps, b);
     }
 
     #[pyfn(m)] #[pyo3(name = "window")]
     fn window_py<'py>(py: Python<'py>, freqs: PyReadonlyArrayDyn<'_, f64>, f_corner: f64, b: f64) {
         let freqs = unsafe { freqs.as_array_mut() };
         smoothing_window(freqs, f_corner, b);
-        // freqs.into_pyarray(py)
-        // freqs
     }
 
     #[pyfn(m)] #[pyo3(name = "__version__")]
