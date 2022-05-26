85816902829106ed5fb7b212435ed5057fd90392
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2022-05-26 23:23:05 +0200

    fix test

--- tests/test_konnoohmachi.py
+++ tests/test_konnoohmachi.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def test_lengths():
-    frequencies = np.linspace(0, 10, 9)
+    frequencies = np.linspace(0, 10, 10)
     amplitudes = np.random.rand(10)
     bandwidth = 40
     konnoohmachi.smooth(frequencies, amplitudes, bandwidth)
